61c10faa1b9ba8980a3a1a0abbc2c8b1f8937596
Rio <rioloka86@gmail.com>
2024-05-11 20:21:51 +0700

    Soal 4 - Adding Explaination of the formula for the speed of parabola motion

---
+++ src/Soal4/KecepatanGerakParabola.java
@@ -0,0 +1,28 @@
+package Soal4;
+
+public class KecepatanGerakParabola {
+    //Gerak Parabola
+    /*Gerak parabola adalah gerak yang membentuk sebuah pola melengkung atau parabola. Fenomena ini terjadi ketika gerak
+     lurus beraturan (GLB) dan gerak lurus berubah beraturan (GLBB) saling berpadu. Kedua jenis gerak ini membentuk
+     sudut elevasi terhadap sumbu x (horizontal) dan sumbu y (vertikal). Sumbu x mewakili GLB, sedangkan sumbu y
+     mencerminkan GLBB. Dalam konteks ini, kombinasi keduanya menghasilkan lintasan melengkung yang kita sebut sebagai
+     gerak parabola.
+
+     Rumus Kecepatan Gerak Parabola:
+     Vt = √Vx^2 + Vy^2
+     Vx = Vo cos alpha
+     Vy = Vo sin alpha - g t
+
+     Ket: Vt = kecepatan parabola sesaat setelah t detik (m/s)
+          Vx = kecepatan di sumbu x (m/s)
+          Vy = kecepatan di sumbu y (m/s)
+          Vo = kecepatan awal ketika t = 0 detik (m/s)
+          alpha = sudut elevasi gerak parabola
+          g = percepatan gaya gravitasi (m/s^2)
+          t = waktu (s)
+
+     Gaya gravitasi (g) apabila tidak diinput nilainya oleh user, maka nilainya akan menjadi 9,8 m/s2
+     Gaya gravitasi dapat berbeda pada setiap tempat di permukaan bumi. Di equator percepatan gravitasi
+     sekitar 9,78 m/s2, sedangkan di daerah kutub sekitar 9,83 m/s2.
+     */
+}
