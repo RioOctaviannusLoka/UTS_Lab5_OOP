a3d7c00c7dd5ebd8b2cc00a7afc765519fd0cf66
Rio <rioloka86@gmail.com>
2024-05-11 17:55:27 +0700

    Soal 3 - Fixing bug on raiseSalary Method

--- src/Soal3/Main.java
+++ src/Soal3/Main.java
@@ -2,21 +2,21 @@ package Soal3;
 
 public class Main {
     public static void main(String[] args) {
-    // Test constructor and toString()
-    Employee e1 = new Employee(8, "Xavier", "Valave", 2500);
-    System.out.println(e1); // toString();
-    // Test Setters and Getters
-    e1.setSalary(999);
-    System.out.println(e1); // toString();
-    System.out.println("id is: " + e1.getID());
-    System.out.println("firstname is: " + e1.getFirstName());
-    System.out.println("lastname is: " + e1.getLastName());
-    System.out.println("salary is: " + e1.getSalary());
-    System.out.println("name is: " + e1.getName());
-    System.out.println("annual salary is: " + e1.getAnnualSalary()); //Test method
+        // Test constructor and toString()
+        Employee e1 = new Employee(8, "Xavier", "Valave", 2500);
+        System.out.println(e1); // toString();
+        // Test Setters and Getters
+        e1.setSalary(999);
+        System.out.println(e1); // toString();
+        System.out.println("id is: " + e1.getID());
+        System.out.println("firstname is: " + e1.getFirstName());
+        System.out.println("lastname is: " + e1.getLastName());
+        System.out.println("salary is: " + e1.getSalary());
+        System.out.println("name is: " + e1.getName());
+        System.out.println("annual salary is: " + e1.getAnnualSalary()); //Test method
 
-    // Test raiseSalary()
-    System.out.println(e1.raiseSalary(10));
-    System.out.println(e1);
-}
+        // Test raiseSalary()
+        System.out.println(e1.raiseSalary(10));
+        System.out.println(e1);
+    }
 }
